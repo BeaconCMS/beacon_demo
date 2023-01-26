9fce0548efc9d15a7653046ea5483cd41bd54c02
Leandro Cesquini Pereira <leandro@leandro.io>
2023-01-26 13:03:47 -0500

    Seeds

--- lib/beacon_demo/release.ex
+++ lib/beacon_demo/release.ex
@@ -18,6 +18,16 @@ defmodule BeaconDemo.Release do
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
+  def beacon_seeds do
+    load_app()
+
+    {:ok, _, _} =
+      Ecto.Migrator.with_repo(Beacon.Repo, fn _repo ->
+        seeds_path = Path.join([:code.priv_dir(@app), "repo", "beacon_seeds.exs"])
+        Code.eval_file(seeds_path)
+      end)
+  end
+
   defp repos do
     Application.fetch_env!(@app, :ecto_repos)
   end
---
+++ rel/overlays/bin/beacon_seeds.bat
@@ -0,0 +1,1 @@
+call "%~dp0\beacon_demo" eval BeaconDemo.Release.beacon_seeds
